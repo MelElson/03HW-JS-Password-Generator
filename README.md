c85a979a15a84fd466a25456772e9bddb86f1594
Melanie Elson <melanie.elson@icloud.com>
2021-03-03 11:54:27 -0700

    provided way to omit selection and added info to readme

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # 03HW-JS-Password-Generator
+[![forthebadge](https://forthebadge.com/images/badges/uses-html.svg)](https://forthebadge.com)
+
+[![forthebadge](https://forthebadge.com/images/badges/uses-css.svg)](https://forthebadge.com)
+
+[![forthebadge](https://forthebadge.com/images/badges/gluten-free.svg)](https://forthebadge.com)
+
+## Table of Contents
+1. [Title](#title)
+2. [General Info](#general-info)
+3. [Technologies](#technologies)
+4. [Installation](#installation)
+5. [Collaboration](#collaboration)
+6. [Steps](#Steps)
+
+
+
+## Title
+Personal portfolio
+
+## General Info
+The purpose of this task is to create password generator that will produce a password based on the user's input. The user will be prompted ti input length of the password then followed by if they want special characters, uppercase, lowercase, and/or numbers.
+
+## Technologies 
+The site uses HTML and CSS code
+
+## Installation
+To access the website go to https://melelson.github.io/02HW-Adv-CSS-Portfolio/
+To clone https://github.com/MelElson/02HW-Adv-CSS-Portfolio
+
+
+# Collaboration 
+Pull requests are encouraged. Please make sure to update as appropriate 
+
+# Steps
+Steps taken by user
+	• User clicks on link and is given a prompt that asks to choose length of password.
+	• The user is then prompted if they would like Special Characters. Pressing ok includes. Pressing Cancel omits.
+	• The user is then prompted if they would like Uppercase. Pressing ok includes. Pressing Cancel omits.
+	• The user is then prompted if they would like Lowercase. Pressing ok includes. Pressing Cancel omits.
+    • The user is then prompted if they would like Numbers. Pressing ok includes. Pressing Cancel omits.
+	• Once user completes prompts they will click on Generate Password and a password will be created based on random values 
+      of the selection they chose within the prompts.
+    
+### Code
+![Changed HTML](./assets/images/code1.png)
+![Changed HTML](./assets/images/code3.png)
+
+### Website Navbar
+![Changed HTML](./assets/images/navbar.png)
+### Examples of Work
+![Changed HTML](./assets/images/examplesofwork.png)
+
+### Website Snapshot
+![Changed HTML](./assets/images/screenshotportfolio.png)
--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 
 
 // Assignment Code
-//var test = prompt("test")
+
+// When I click on Generate Password Button
 var generateBtn = document.querySelector("#generate");
+
+// Declare variables
 var special = ["!","#","$","%","&","*","+","-",".","/",":",";","<","=",">","?","@"];
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -12,6 +15,7 @@ var password = ""
 var options = []
 var guarenteedOptions = []
 var endPassword = []
+
 // start
 var generatePassword = function() {
   
@@ -24,35 +28,40 @@ var generatePassword = function() {
 
   //do I want special characters
  var userChoiceSpecChar = window.confirm("Do you want SPECIAL CHARACTERS for your password?");
-  if (userChoiceSpecChar == true) {
+  if (userChoiceSpecChar == true || userChoiceSpecChar ==null) {
     options = options.concat(special) 
     guarenteedOptions.push(random(special))
-  }
+    userChoiceSpecChar = "";
+  } 
+    
 
   //Do I want upppercase
   var userChoiceUpper = window.confirm("Do you want UPPERCASE for your password?");
-  if (userChoiceUpper == true) {
+  if (userChoiceUpper == true || userChoiceUpper ==null) {
     options = options.concat(upper)
     guarenteedOptions.push(random(upper))
-  }
+    userChoiceUpper ="";
+  } 
 
   //Do I want lowercase
 
   var userChoicelower = window.confirm("Do you want LOWERCASE for your password?");
-  if (userChoicelower == true) {
+  if (userChoicelower == true || userChoicelower ==null) {
     options = options.concat(lower)
     guarenteedOptions.push(random(lower))
-  }
+    userChoicelower ="";
+  } 
   //Do I want number 
   var userChoiceNumber = window.confirm("Do you want NUMBERS for your password?");
-  if (userChoiceNumber == true) {
+  if (userChoiceNumber == true || userChoiceNumber ==null) {
     options = options.concat(number) 
     guarenteedOptions.push(random(number))
-  }
+    userChoiceNumber ="";
+  } 
 
   
 
-
+//for statement for random options for password
 for (i=0;i<length;i++){
   var randomChar = random(options)
   endPassword.push(randomChar)
@@ -77,7 +86,7 @@ function random(arr){
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+// Display results in textbox
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
